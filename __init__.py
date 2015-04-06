eb8dc09a8b10854b7a68dbcf27b2e3cb6415a81f
Angus Hollands <goosey15@gmail.com>
2015-04-06 05:02:59 +0100

    Fix file error

--- __init__.py
+++ __init__.py
@@ -159,7 +159,6 @@ class AttributeGroup(bpy.types.PropertyGroup):
     name = bpy.props.StringProperty()
     type = bpy.props.StringProperty()
 
-    notify = bpy.props.BoolProperty(default=False, description="Whether attribute should trigger notifications")
     replicate = bpy.props.BoolProperty(default=False, description="Replicate this attribute")
 
     replicate_for_owner = bpy.props.BoolProperty(default=False, description="Replicate this attribute to the owner "
@@ -620,7 +619,6 @@ class RENDER_RT_AttributeList(bpy.types.UIList):
 
         item_active = item.replicate
 
-        row.prop(item, "notify", text="", icon='INFO')
         row.prop(item, "replicate_for_owner", text="", icon='LOOP_BACK')
         row.prop(item, "replicate_after_initial", text="", icon='DOTSUP')
         row.active = item_active
